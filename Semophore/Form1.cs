e10276e18c86ad19b308c32d7af6d23350ce31e1
Haqve_vu <novruzluqyds20@gmail.com>
2023-11-18 01:14:33 -0800

    Completed

--- Semophore/Form1.cs
+++ Semophore/Form1.cs
@@ -14,7 +14,7 @@ namespace Semophore
         public ObservableCollection<Thread> Working_List = new ObservableCollection<Thread>();
         public ObservableCollection<string> Working_Names = new ObservableCollection<string>();
 
-        SemaphoreSlim semaphoreSlim = new SemaphoreSlim(6,6);
+        SemaphoreSlim semaphoreSlim = new SemaphoreSlim(0, 6);
         public Form1()
         {
             InitializeComponent();
@@ -78,36 +78,33 @@ namespace Semophore
                 }
             }
 
+            await Task.Delay(5000);
             for (int i = 0; i < Waiting_List.Count; i++)
             {
                 Waiting_List[i].Start();
-                await Task.Delay(2000);
                 Waiting_Names.Remove(Waiting_List[i].Name);
                 Working_Names.Add(Waiting_List[i].Name);
                 Working_List.Add(Waiting_List[i]);
                 Waiting_List.Remove(Waiting_List[i]);
 
                 Working_Threads.DataSource = null;
-                Working_Threads.DataSource = Working_Names ;
+                Working_Threads.DataSource = Working_Names;
 
                 Waiting_Threads.DataSource = null;
                 Waiting_Threads.DataSource = Waiting_Names;
             }
 
-            for (int i = 0; i < Working_List.Count; i++)
-            {
-                if (Working_List[i].ThreadState is ThreadState.Stopped)
+               await Task.Delay(1000);
+                for (int i = 0; i < Working_List.Count; i++)
                 {
-                    await Task.Delay(2000);
 
-                    Working_Names.RemoveAt(i);
-                    Working_Threads.DataSource = null;
-                    Working_Threads.DataSource = Working_Names;
+                        Working_Names.Remove(Working_List[i].Name);
+                        Working_Threads.DataSource = null;
+                        Working_Threads.DataSource = Working_Names;
 
-                    Working_List.RemoveAt(i);
-
+                        Working_List.RemoveAt(i);
                 }
-            }
+            
 
         }
 
